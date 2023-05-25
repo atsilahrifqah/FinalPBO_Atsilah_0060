3100a1386457c4b476c0276f47339a039dcea584
atsilahrifqah <127533470+atsilahrifqah@users.noreply.github.com>
2023-05-26 07:54:32 +0800

    Create inventory.java

---
+++ inventory/inventory.java
@@ -0,0 +1,18 @@
+import inventory.View.View_Inventory;
+
+/**
+ *
+ * @author Kyy
+ */
+public class Inventory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        new View_Inventory().setVisible(true);
+    }
+    
+}
